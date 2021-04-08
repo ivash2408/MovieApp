91be319ab1f60e2c1241688c74c90be5944fd1e7
Ivash2408 <ivashka24011@gmail.com>
2021-04-08 14:10:32 +0300

    Hometask Fragments Initial

---
+++ app/src/main/java/com/ivash/movieapp/FragmentMovieDetails.kt
@@ -0,0 +1,15 @@
+package com.ivash.movieapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FragmentMovieDetails : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_movie_details, container, false)
+
+    }
+}
