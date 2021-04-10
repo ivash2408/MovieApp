b0f24dca6121032579b2d90aa62811f16d403d5d
Ivash2408 <ivashka24011@gmail.com>
2021-04-10 12:22:55 +0300

    Add Cicerone Navigation to Main Activity

--- app/src/main/java/com/ivash/movieapp/FragmentMoviesList.kt
+++ app/src/main/java/com/ivash/movieapp/FragmentMoviesList.kt
@@ -1,10 +1,46 @@
 package com.ivash.movieapp
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import com.ivash.movieapp.databinding.FragmentMoviesListBinding
+import ru.terrakok.cicerone.Cicerone
+import ru.terrakok.cicerone.Navigator
+import ru.terrakok.cicerone.android.pure.AppNavigator
+import ru.terrakok.cicerone.android.support.SupportAppNavigator
 
 class FragmentMoviesList : Fragment(R.layout.fragment_movies_list) {
+    private var _binding: FragmentMoviesListBinding? = null
+    private val binding get() = _binding!!
+
+    private val cicerone = Cicerone.create()
+    private val navHolder = cicerone.navigatorHolder
+    private val router = cicerone.router
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMoviesListBinding.inflate(inflater, container, false)
+
+        binding.movieCard.setOnClickListener {openMovieDescription()}
+        return binding.root
+    }
+
+    fun openMovieDescription() {
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
--- app/src/main/java/com/ivash/movieapp/MainActivity.kt
+++ app/src/main/java/com/ivash/movieapp/MainActivity.kt
@@ -1,25 +1,56 @@
 package com.ivash.movieapp
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ivash.movieapp.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import ru.terrakok.cicerone.Cicerone
+import ru.terrakok.cicerone.Navigator
+import ru.terrakok.cicerone.NavigatorHolder
+import ru.terrakok.cicerone.Router
+import ru.terrakok.cicerone.android.pure.AppNavigator
+import ru.terrakok.cicerone.android.support.SupportAppNavigator
+import ru.terrakok.cicerone.commands.Command
 
 class MainActivity : AppCompatActivity() {
-    private val cicerone = Cicerone.create()
-    val router get() = cicerone.router
-    val navigatorHolder get() = cicerone.getNavigatorHolder()
 
     private lateinit var binding: ActivityMainBinding
+
+    private val cicerone = Cicerone.create()
+    private val navHolder = cicerone.navigatorHolder
+    private val router = cicerone.router
+
+    private val navigator: Navigator = SupportAppNavigator(this, R.id.main_container)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.main_container, FragmentMoviesList())
-                .commit()
+        INSTANCE = this
+    }
+
+    override fun onResume() {
+        super.onResume()
+        navHolder.setNavigator(navigator)
+        val screen = MovieDetailsScreen
+        router.navigateTo(screen)
+    }
+
+    override fun onPause() {
+        navHolder.removeNavigator()
+        super.onPause()
+    }
+
+    companion object {
+        internal lateinit var INSTANCE: MainActivity
+            private set
+
+        fun getIntent(context: Context?) = Intent(context, MainActivity::class.java)
     }
 }
