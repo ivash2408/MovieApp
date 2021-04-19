9a220adcfde51bf20fe4ce5738942483d80c6417
Ivash2408 <ivashka24011@gmail.com>
2021-04-19 18:06:00 +0300

    fixes

--- app/src/main/java/com/ivash/movieapp/FragmentMovieDetails.kt
+++ app/src/main/java/com/ivash/movieapp/FragmentMovieDetails.kt
@@ -1,13 +1,10 @@
 package com.ivash.movieapp
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.ivash.movieapp.databinding.FragmentMovieDetailsBinding
-import com.ivash.movieapp.databinding.FragmentMoviesListBinding
 
 class FragmentMovieDetails : Fragment(R.layout.fragment_movie_details) {
     private val binding by viewBinding(FragmentMovieDetailsBinding::bind)
